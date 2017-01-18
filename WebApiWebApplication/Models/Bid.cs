02a2a24c1be8c15fe93882af87ad4adc8e28c09b
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2017-01-18 13:14:14 +0100

    Took most of the models' code from my WCF practice exam project

---
+++ WebApiWebApplication/Models/Bid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebApiWebApplication.Models
+{
+    public class Bid
+    {
+        public int ItemNumber { get; set; }
+        public decimal Price { get; set; }
+        public string CustomName { get; set; }
+        public int CustomPhone { get; set; }
+
+        public Bid(int itemNumber, decimal price, string customName, int customPhone)
+        {
+            ItemNumber = itemNumber;
+            Price = price;
+            CustomName = customName;
+            CustomPhone = customPhone;
+        }
+    }
+}
